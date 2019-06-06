d779da922f1edebaa0bed842360585fa404aa085
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2019-06-06 17:47:31 +0900

    [fitParamDelayInputModel.py] output optimized delay and time constant for both steer angle and velocity Add CUTOFF_TIME parameter -- [0:CUTOFF_TIME] will not be consider

--- ros/src/computing/planning/motion/packages/waypoint_follower/nodes/wf_simulator/fitParamDelayInputModel.py
+++ ros/src/computing/planning/motion/packages/waypoint_follower/nodes/wf_simulator/fitParamDelayInputModel.py
@@ -6,10 +6,9 @@ import argparse
 import pandas as pd
 
 FREQ_SAMPLE = 0.001
+CUTOFF_TIME = 15.0
 
-def getActValue(path, speed_type):
-    # Read data
-    df = pd.read_csv(path, sep=' ')
+def getActValue(df, speed_type):
     tm = np.array(list(df['%time'])) * 1e-9
     # Unit Conversion
     if speed_type:
@@ -20,9 +19,7 @@ def getActValue(path, speed_type):
     dval = (val[2:] - val[:-2]) / (tm[2:] - tm[:-2])
     return tm[1:-1], val[1:-1], dval
 
-def getCmdValueWithDelay(path, delay):
-    # Read data
-    df = pd.read_csv(path, sep=' ')
+def getCmdValueWithDelay(df, delay):
     tm = np.array(list(df['%time'])) * 1e-9
     val = np.array(list(df['field']))
     return tm + delay, val
@@ -35,20 +32,21 @@ def getLinearInterpolate(_tm, _val, _index, ti):
     val_i = tmp_val + (tmp_nextval - tmp_val) / (tmp_nextt - tmp_t) * (ti - tmp_t)
     return val_i
 
-def getFittingTimeConstantParam(cmd_log, act_log, delay, speed_type = False):
-    tm_cmd, cmd_delay = getCmdValueWithDelay(cmd_log, delay)
-    tm_act, act, dact = getActValue(act_log, speed_type)
+def getFittingTimeConstantParam(cmd_data, act_data, \
+                                delay, speed_type = False):
+    tm_cmd, cmd_delay = getCmdValueWithDelay(cmd_data, delay)
+    tm_act, act, dact = getActValue(act_data, speed_type)
     _t_min = max(tm_cmd[0], tm_act[0])
     _t_max = min(tm_cmd[-1], tm_act[-1])
     tm_cmd = tm_cmd - _t_min
     tm_act = tm_act - _t_min
-    MAX_CNT = int((_t_max - _t_min) / FREQ_SAMPLE)
+    MAX_CNT = int((_t_max - _t_min - CUTOFF_TIME) / FREQ_SAMPLE)
     dact_samp = [None] * MAX_CNT
     diff_actcmd_samp = [None] * MAX_CNT
     ind_cmd = 0
     ind_act = 0
     for ind in range(MAX_CNT):
-        ti = ind * FREQ_SAMPLE
+        ti = ind * FREQ_SAMPLE + CUTOFF_TIME
         while (tm_cmd[ind_cmd + 1] < ti):
             ind_cmd += 1
         cmd_delay_i = getLinearInterpolate(tm_cmd, cmd_delay, ind_cmd, ti)
@@ -68,14 +66,14 @@ MIN_DELAY = 0.0
 MAX_DELAY = 2.0
 DELAY_INCR = 0.1
 
-def getFittingParam(cmd_log, act_log, speed_type = False):
+def getFittingParam(cmd_data, act_data, speed_type = False):
     delay_range = int((MAX_DELAY - MIN_DELAY) / DELAY_INCR)
     delays = [MIN_DELAY + i * DELAY_INCR for i in range(delay_range + 1)]
     error_min = 1.0e10
     delay_opt = -1
     tau_opt = -1
     for delay in delays:
-        tau, error = getFittingTimeConstantParam(cmd_log, act_log, delay, speed_type=speed_type)
+        tau, error = getFittingTimeConstantParam(cmd_data, act_data, delay, speed_type=speed_type)
         if error < error_min:
             error_min = error
             delay_opt = delay
@@ -83,14 +81,17 @@ def getFittingParam(cmd_log, act_log, speed_type = False):
     return tau_opt, delay_opt, error_min
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Paramter fitting for Input Delay Model (First Order System with Dead Time)')
-    parser.add_argument('--cmd_log', '-c', required=True, help='Vehicle command log file')
-    parser.add_argument('--act_log', '-a', required=True, help='Vehicle actual status log file')
-    parser.add_argument('--speed_type', '-s', choices=['True', 'False'], \
-                        default='False', help='Type speed or not: Steering Angle = False, velocity = True')
-    args = parser.parse_args()
-    if args.speed_type == 'True':
-        tau_opt, delay_opt, error = getFittingParam(args.cmd_log, args.act_log, speed_type=True)
-    else:
-        tau_opt, delay_opt, error = getFittingParam(args.cmd_log, args.act_log, speed_type=False)
-    print tau_opt, delay_opt, error
+    steer_cmd_log = 'vehicle_test_20190218-164427.cmd_steering_angle'
+    steer_act_log = 'vehicle_test_20190218-164427.vehicle_status_angle'
+    vel_cmd_log = 'vehicle_test_20190218-164427.cmd_linear_velocity'
+    vel_act_log = 'vehicle_test_20190218-164427.vehicle_status_speed'
+    steer_cmd_data = pd.read_csv(steer_cmd_log, sep=' ')
+    steer_act_data = pd.read_csv(steer_act_log, sep=' ')
+    vel_cmd_data = pd.read_csv(vel_cmd_log, sep=' ')
+    vel_act_data = pd.read_csv(vel_act_log, sep=' ')
+    tau_opt, delay_opt, error = getFittingParam(steer_cmd_data, \
+                                                steer_act_data, speed_type=False)
+    print ('Steer angle: tau_opt = %2.4f, delay_opt = %2.4f, error = %2.4e' %(tau_opt, delay_opt, error))
+    tau_opt, delay_opt, error = getFittingParam(vel_cmd_data, \
+                                                vel_act_data, speed_type=True)
+    print ('Velocity:    tau_opt = %2.4f, delay_opt = %2.4f, error = %2.4e' %(tau_opt, delay_opt, error))
