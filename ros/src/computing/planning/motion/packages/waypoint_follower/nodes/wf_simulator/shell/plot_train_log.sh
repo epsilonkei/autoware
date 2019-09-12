282a76669db2800f30c8641be753ae9abc034fe2
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2019-09-12 17:28:21 +0900

    Add plot_train_log scripts

---
+++ ros/src/computing/planning/motion/packages/waypoint_follower/nodes/wf_simulator/shell/plot_train_log.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ ! "$XRANGE" ]; then XRANGE="[*:*]"; fi
+if [ ! "$YRANGE" ]; then YRANGE="[*:*]"; fi
+
+echo "XRANGE=$XRANGE";
+echo "YRANGE=$YRANGE";
+
+gnuplot <<EOF
+# set terminal postscript eps color enhanced
+set terminal png
+set tics font "Times New Roman,16"
+set xlabel font "Times New Roman,16"
+set ylabel font "Times New Roman,16"
+set zlabel font "Times New Roman,16"
+set key font "Times New Roman,16"
+set key right top
+set key width 3
+# set output "train_log.eps"
+set output "train_log.png"
+set grid
+set size ratio 0.5
+set xlabel "Epoch"
+set ylabel "Loss"
+set xrange ${XRANGE}
+set title "_"
+set logscale y
+onlySim_vel_loss(x) = 1.286722e-02
+onlySim_steer_loss(x) = 3.167539e-05
+plot "train_log.txt" using 1:2 with line linewidth 2 title "VelLoss", "train_log.txt" using 1:3 with line linewidth 2 title "SteerLoss", onlySim_vel_loss(x) with lines linewidth 2 title "OnlySimulateVelLoss", onlySim_steer_loss(x) with lines linewidth 2 title "OnlySimulateSteerLoss"
+EOF
