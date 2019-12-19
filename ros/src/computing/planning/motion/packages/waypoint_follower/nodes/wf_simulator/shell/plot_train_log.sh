adf87bfd1c99ae93371c543fac45a9d737722078
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2019-12-19 15:07:10 +0900

    [shell/plot_train_log.sh] Add plot for test_log

--- ros/src/computing/planning/motion/packages/waypoint_follower/nodes/wf_simulator/shell/plot_train_log.sh
+++ ros/src/computing/planning/motion/packages/waypoint_follower/nodes/wf_simulator/shell/plot_train_log.sh
@@ -6,7 +6,7 @@ if [ ! "$YRANGE" ]; then YRANGE="[*:*]"; fi
 echo "XRANGE=$XRANGE";
 echo "YRANGE=$YRANGE";
 
-gnuplot <<EOF
+_DEFGNUP=`cat <<EOF
 # set terminal postscript eps color enhanced
 set terminal png size 1280, 900
 set tics font "Times New Roman,16"
@@ -16,8 +16,6 @@ set zlabel font "Times New Roman,16"
 set key font "Times New Roman,16"
 set key right top
 set key width 3
-# set output "train_log.eps"
-set output "train_log.png"
 set grid
 set size ratio 0.5
 set xlabel "Epoch"
@@ -28,5 +26,18 @@ set logscale y
 onlySim_vel_loss(x) = 2.424530e-02
 onlySim_steer_loss(x) = 2.485642e-05
 onlySim_dsteer_loss(x) = 1.847670e-03
+EOF`
+
+gnuplot <<EOF
+${_DEFGNUP}
+# set output "train_log.eps"
+set output "train_log.png"
 plot "train_log.txt" using 1:2 with line linewidth 2 title "VelLoss", "train_log.txt" using 1:3 with line linewidth 2 title "SteerLoss", onlySim_vel_loss(x) with lines linewidth 2 title "OnlySimulateVelLoss", onlySim_steer_loss(x) with lines linewidth 2 title "OnlySimulateSteerLoss", "train_log.txt" using 1:4 with line linewidth 2 title "dSteerLoss", onlySim_dsteer_loss(x) with lines linewidth 2 title "OnlySimulateDsteerLoss"
+EOF
+
+gnuplot <<EOF
+${_DEFGNUP}
+# set output "test_log.eps"
+set output "test_log.png"
+plot "test_log.txt" using 1:2 with line linewidth 2 title "VelLoss", "test_log.txt" using 1:3 with line linewidth 2 title "SteerLoss", onlySim_vel_loss(x) with lines linewidth 2 title "OnlySimulateVelLoss", onlySim_steer_loss(x) with lines linewidth 2 title "OnlySimulateSteerLoss", "test_log.txt" using 1:4 with line linewidth 2 title "dSteerLoss", onlySim_dsteer_loss(x) with lines linewidth 2 title "OnlySimulateDsteerLoss"
 EOF
